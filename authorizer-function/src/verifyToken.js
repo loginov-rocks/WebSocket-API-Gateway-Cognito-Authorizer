d57fa1080a67e57b1261e551d97b727f33067680
Danila Loginov <danila@loginov.rocks>
2024-01-21 15:41:43 -0800

    Authorizer Function: Refactor verifyToken module

--- authorizer-function/src/verifyToken.js
+++ authorizer-function/src/verifyToken.js
@@ -1,36 +1,56 @@
 import { decode, verify } from 'jsonwebtoken';
 import jwkToPem from 'jwk-to-pem';
 
-// TODO: Refactor, add caching.
-const getPemEncodedPublicKey = async (region, userPoolId, keyId) => {
-  const response = await fetch(`https://cognito-idp.${region}.amazonaws.com/${userPoolId}/.well-known/jwks.json`);
-  const json = await response.json();
+// Simple in-memory cache, optional. In this Lambda it will always have a single promise stored because Region and User
+// Pool ID are taken from the environment variables and remain unchanged within the life cycle of the Lambda, but I
+// still kept this flexible in case someone will copy the code. Subject for improvement.
+const getPemEncodedPublicKeysPromisesCache = new Map();
 
-  const pemEncodedPublicKeys = {};
+const getPemEncodedPublicKeysImplementation = async (jwksUrl) => {
+  console.log('jwksUrl', jwksUrl);
+
+  const response = await fetch(jwksUrl);
+
+  if (!response.ok) {
+    throw new Error(`Unable to fetch JWKS using URL ${jwksUrl}!`);
+  }
+
+  const json = await response.json();
+  const pemEncodedPublicKeys = new Map();
 
   json.keys.forEach(({ kid, kty, n, e }) => {
-    pemEncodedPublicKeys[kid] = jwkToPem({
-      kty: kty,
-      e: e,
-      n: n,
-    });
+    pemEncodedPublicKeys.set(kid, jwkToPem({ kty, e, n }));
   });
 
-  return pemEncodedPublicKeys[keyId] || null;
-}
+  return pemEncodedPublicKeys;
+};
+
+const getPemEncodedPublicKeys = (region, userPoolId) => {
+  const jwksUrl = `https://cognito-idp.${region}.amazonaws.com/${userPoolId}/.well-known/jwks.json`;
+
+  if (!getPemEncodedPublicKeysPromisesCache.has(jwksUrl)) {
+    // The implementation is executed and set as the map element immediately to decrease the chances of the race
+    // conditions' impact (the same function triggered in parallel, but not yet resolved). Subject for improvement.
+    getPemEncodedPublicKeysPromisesCache.set(jwksUrl, getPemEncodedPublicKeysImplementation(jwksUrl));
+  }
+
+  return getPemEncodedPublicKeysPromisesCache.get(jwksUrl);
+};
 
 export const verifyToken = async (region, userPoolId, token) => {
   var decodedJwt = decode(token, { complete: true });
 
-  if (!decodedJwt) {
+  if (!decodedJwt || !decodedJwt.header || !decodedJwt.header.kid) {
     return null;
   }
 
-  const { kid } = decodedJwt.header;
-  const pemEncodedPublicKey = await getPemEncodedPublicKey(region, userPoolId, kid);
+  const pemEncodedPublicKeys = await getPemEncodedPublicKeys(region, userPoolId);
+  const pemEncodedPublicKey = pemEncodedPublicKeys.get(decodedJwt.header.kid);
 
   if (!pemEncodedPublicKey) {
-    return null;
+    throw new Error(
+      `Public key ID "${decodedJwt.header.kid}" not found for User Pool ID "${userPoolId}" in "${region}" region!`,
+    );
   }
 
   return verify(token, pemEncodedPublicKey);
